875114d2701524b19d0940cfa98390b4a65e422e
robbyfa71 <robbyfirdauzy@gmail.com>
2024-08-07 23:36:09 +0700

    update: sidebar component to layout components

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body>
-    <x-Sidebar>
+    <x-layout>
         <h1 class="text-2xl font-bold border-b border-orange-200 uppercase">{{ $title }}</h1>
         <div class="grid grid-cols-3 gap-3 mt-5">
-            <x-stats-card>
-                <x-slot:extra>Income</x-slot>
+            <x-stats-card >
                 Rp {{$income}}
             </x-stats-card>
             <x-stats-card>Rp {{$spending}}</x-stats-card>
             <x-stats-card>Rp {{$margin}}</x-stats-card>
         </div>
-    </x-Sidebar>
-</body>
+    </x-layout>
 
-</html>
--- resources/views/admin/income.blade.php
+++ resources/views/admin/income.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pemasukan</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
-</head>
-
-<body>
-    <x-Sidebar>
+    <x-layout>
         <h1 class="font">{{$title}}</h1>
-    </x-Sidebar>
-</body>
-
-</html>
+    </x-layout>
